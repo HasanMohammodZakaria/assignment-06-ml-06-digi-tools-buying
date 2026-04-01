fc4139fe3548f68a47352c80c1b851585885351b
Hasan Md Zakaria <zakariak4@gmail.com>
2026-04-01 14:52:17 +0600

    Pricing Card resonsive all device

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -5,28 +5,28 @@ import rocket from '../assets/rocket.png';
 
 const Getstarted = () => {
     return (
-        <div className='bg-[#F9FAFC] mt-20 py-30 border-b-3 border-gray-100'>
-            <h3 className='text-[48px] text-[#101727] font-extrabold text-center mt-4'>Get Started in 3 Steps</h3>
-            <p className='text-[#627382] leading-5 text-center'>Start using premium digital tools in minutes, not hours.</p>
+        <div className='bg-[#F9FAFC] mt-10 py-20 md:py-28 lg:py-32 border-b border-gray-100'>
+            <h3 className='text-3xl md:text-4xl lg:text-[48px] text-[#101727] font-extrabold text-center'>Get Started in 3 Steps</h3>
+            <p className='text-[#627382] leading-6 text-center mt-2 md:mt-4'>Start using premium digital tools in minutes, not hours.</p>
 
-            <div className='max-w-7xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10'>
-                <div className='relative flex flex-col items-center justify-center rounded-xl bg-white space-y-4 shadow-sm px-6 py-22 '>
+            <div className='max-w-7xl mx-auto grid gap-6 md:gap-8 lg:gap-10 mt-8 md:mt-10 md:grid-cols-2 lg:grid-cols-3'>
+                <div className='relative flex flex-col items-center justify-center rounded-xl bg-white shadow-lg px-6 py-16 md:py-20 space-y-4 '>
                     <img src={user} alt="" />
-                    <h4 className='text-2xl text-[#101727] font-bold'>Create Account</h4>
-                    <p className='text-center text-[#627382] leading-5'>Sign up for free in seconds. No credit card required to get started.</p>
-                    <div className='absolute top-5 right-5 w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center text-[14px] text-white font-bold '>01</div>
+                    <h4 className='text-xl md:text-2xl text-[#101727] font-bold text-center'>Create Account</h4>
+                    <p className='text-center text-[#627382] leading-5 text-sm md:text-base'>Sign up for free in seconds. No credit card required to get started.</p>
+                    <div className='absolute top-4 right-4 w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center text-sm text-white font-bold '>01</div>
                 </div> 
-                <div className='relative flex flex-col items-center justify-center rounded-xl bg-white space-y-4 shadow-sm px-6 py-22 '>
+                <div className='relative flex flex-col items-center justify-center rounded-xl bg-white shadow-lg px-6 py-16 md:py-20 space-y-4'>
                     <img src={pack} alt="" />
-                    <h4 className='text-2xl text-[#101727] font-bold'>Choose Products</h4>
-                    <p className='text-center text-[#627382] leading-5'>Browse our catalog and select the toolsthat fit your needs.</p>
-                    <div className='absolute top-5 right-5 w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center text-[14px] text-white font-bold '>02 </div>
+                    <h4 className='text-xl md:text-2xl text-[#101727] font-bold text-center'>Choose Products</h4>
+                    <p className='text-center text-[#627382] leading-5 text-sm md:text-base'>Browse our catalog and select the toolsthat fit your needs.</p>
+                    <div className='absolute top-4 right-4 w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center text-sm text-white font-bold'>02 </div>
                 </div> 
-                <div className='relative flex flex-col items-center justify-center rounded-xl bg-white space-y-4 shadow-sm px-6 py-22 '>
+                <div className='relative flex flex-col items-center justify-center rounded-xl bg-white shadow-lg px-6 py-16 md:py-20 space-y-4 '>
                     <img src={rocket} alt="" />
-                    <h4 className='text-2xl text-[#101727] font-bold'>Start Creating</h4>
-                    <p className='text-center text-[#627382] leading-5'>Download and start using your premium tools immediately.</p>
-                    <div className='absolute top-5 right-5 w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center text-[14px] text-white font-bold '>03</div>
+                    <h4 className='text-xl md:text-2xl text-[#101727] font-bold text-center'>Start Creating</h4>
+                    <p className='text-center text-[#627382] leading-5 text-sm md:text-base'>Download and start using your premium tools immediately.</p>
+                    <div className='absolute top-4 right-4 w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center text-sm text-white font-bold'>03</div>
                 </div> 
                 
             </div>
--- src/components/Pricingcard.jsx
+++ src/components/Pricingcard.jsx
@@ -3,139 +3,97 @@ import { Check } from "lucide-react";
 
 const Pricingcard = () => {
   return (
-    <div className="py-30 max-w-7xl mx-auto">
-      <h3 className="text-[48px] text-[#101727] font-extrabold text-center mt-4">
+    <div className="py-16 md:py-20 lg:py-24 max-w-7xl mx-auto px-4">
+      <h3 className="text-2xl md:text-3xl lg:text-4xl xl:text-[48px] text-[#101727] font-extrabold text-center mb-4">
         Simple, Transparent Pricing
       </h3>
-      <p className="text-[#627382] leading-5 text-center mb-6">
+      <p className="text-sm md:text-base lg:text-lg text-[#627382] leading-5 md:leading-6 text-center mb-10">
         Choose the plan that fits your needs. Upgrade or downgrade anytime.
       </p>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10">
-        <div className=" bg-[#F2F2F2] shadow-md p-6 rounded-lg">
-          <h4 className="text-2xl text-[#101727] font-bold mb-3">Starter</h4>
-          <p className="text-[#627382] leading-5 mb-6">
-            Perfect for getting started
-          </p>
-          <h2 className="text-[40px] font-bold text-[#101727] mb-6">
-            $0<span className="text-[20px] text-[#627382]">/Month</span>
-          </h2>
-          <div className="mb-21">
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                Access to 10 free tools
-              </p>
-            </div>
-            <div className="flex gap-1 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                Basic templates
-              </p>
-            </div>
-            <div className="flex gap-1 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                Community support
-              </p>
-            </div>
-            <div className="flex gap-1 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                1 project per month
-              </p>
+
+     
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-7 lg:gap-8">
+       
+        <div className="flex flex-col justify-between bg-[#F2F2F2] shadow-md p-5 md:p-6 rounded-lg">
+          <div>
+            <h4 className="text-xl md:text-2xl font-bold text-[#101727] mb-2">Starter</h4>
+            <p className="text-[#627382] mb-4">Perfect for getting started</p>
+            <h2 className="text-2xl md:text-4xl font-bold text-[#101727] mb-4">
+              $0<span className="text-[14px] md:text-[20px] text-[#627382]">/Month</span>
+            </h2>
+            <div className="space-y-2">
+              {["Access to 10 free tools", "Basic templates", "Community support", "1 project per month"].map((item, idx) => (
+                <div key={idx} className="flex gap-2 items-start">
+                  <Check className="text-[#30B868] mt-1" />
+                  <p className="text-[#627382] font-medium">{item}</p>
+                </div>
+              ))}
             </div>
           </div>
-          <button className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-[16px] font-semibold text-white px-5 py-3 rounded-4xl w-full mt-">
+          <button className="mt-4 w-full py-3 rounded-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-semibold">
             Get Started Free
           </button>
         </div>
-        <div className="relative bg-linear-to-r from-[#4F39F6] to-[#9514FA] t shadow-md p-6 rounded-lg">
-          <h4 className="text-2xl text-white font-bold mb-3">Pro</h4>
-          <p className="text-white leading-5 mb-6">
-            Best for professionals
-          </p>
-          <h2 className="text-[40px] font-bold text-white mb-6">
-            $29<span className="text-[20px] text-white">/Month</span>
-          </h2>
-          <div className="mb-6">
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-white" />
-              <p className="text-white leading-5 font-medium">
-                Access to all premium tools
-              </p>
+
+        
+        <div className="flex flex-col justify-between relative bg-linear-to-r from-[#4F39F6] to-[#9514FA] shadow-md p-5 md:p-6 rounded-lg">
+          <div>
+            <h4 className="text-xl md:text-2xl font-bold text-white mb-2">Pro</h4>
+            <p className="text-white mb-4">Best for professionals</p>
+            <h2 className="text-2xl md:text-4xl font-bold text-white mb-4">
+              $29<span className="text-[14px] md:text-[20px] text-white">/Month</span>
+            </h2>
+            <div className="space-y-2">
+              {[
+                "Access to all premium tools",
+                "Unlimited templates",
+                "Priority support",
+                "Unlimited projects",
+                "Cloud sync",
+                "Advanced analytics",
+              ].map((item, idx) => (
+                <div key={idx} className="flex gap-2 items-start">
+                  <Check className="text-white mt-1" />
+                  <p className="text-white font-medium">{item}</p>
+                </div>
+              ))}
             </div>
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-white" />
-              <p className="text-white leading-5 font-medium">
-                Unlimited templates
-              </p>
-            </div>
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-white" />
-              <p className="text-white leading-5 font-medium">
-                Priority support
-              </p>
-            </div>
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-white" />
-              <p className="text-white leading-5 font-medium">
-                Unlimited projects
-              </p>
-            </div>
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-white" />
-              <p className="text-white leading-5 font-medium">
-                Cloud sync
-              </p>
-            </div>
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-white" />
-              <p className="text-white leading-5 font-medium">
-               Advanced analytics
-              </p>
-            </div>
-            
           </div>
-          <button className="bg-white text-[16px] font-semibold text-white px-5 py-3 rounded-4xl w-full mt-">
-           <span className="text-[16px] font-semibold whitespace-nowrap bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">Start Pro Trial</span>
+          <button className="mt-4 w-full py-3 rounded-4xl bg-white">
+            <span className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-semibold text-[16px]">
+              Start Pro Trial
+            </span>
           </button>
-          <div className="absolute left-1/2 -translate-x-1/2 -top-5  badge bg-[#FEF3C6] px-3 py-4 rounded-4xl text-[#BB4D00]">Most Popular</div>
+          <div className="absolute left-1/2 -translate-x-1/2 -top-5 px-3 py-1.5 rounded-4xl bg-[#FEF3C6] text-[#BB4D00] text-sm md:text-base">
+            Most Popular
           </div>
-        <div className=" bg-[#F2F2F2] shadow-md p-6 rounded-lg">
-          <h4 className="text-2xl text-[#101727] font-bold mb-3">Starter</h4>
-          <p className="text-[#627382] leading-5 mb-6">
-            Perfect for getting started
-          </p>
-          <h2 className="text-[40px] font-bold text-[#101727] mb-6">
-            $0<span className="text-[20px] text-[#627382]">/Month</span>
-          </h2>
-          <div className="mb-21">
-            <div className="flex gap-1.5 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                Access to 10 free tools
-              </p>
-            </div>
-            <div className="flex gap-1 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                Basic templates
-              </p>
-            </div>
-            <div className="flex gap-1 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                Community support
-              </p>
-            </div>
-            <div className="flex gap-1 mb-1">
-              <Check className="text-[#30B868]" />
-              <p className="text-[#627382] leading-5 font-medium">
-                1 project per month
-              </p>
+        </div>
+
+       
+        <div className="flex flex-col justify-between bg-[#F2F2F2] shadow-md p-5 md:p-6 rounded-lg">
+          <div>
+            <h4 className="text-xl md:text-2xl font-bold text-[#101727] mb-2">Enterprise</h4>
+            <p className="text-[#627382] mb-4">For teams and businesses</p>
+            <h2 className="text-2xl md:text-4xl font-bold text-[#101727] mb-4">
+              $99<span className="text-[14px] md:text-[20px] text-[#627382]">/Month</span>
+            </h2>
+            <div className="space-y-2">
+              {[
+                "Everything in Pro",
+                "Team collaboration",
+                "Custom integrations",
+                "Dedicated support",
+                "SLA guarantee",
+                "Custom branding",
+              ].map((item, idx) => (
+                <div key={idx} className="flex gap-2 items-start">
+                  <Check className="text-[#30B868] mt-1" />
+                  <p className="text-[#627382] font-medium">{item}</p>
+                </div>
+              ))}
             </div>
           </div>
-          <button className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-[16px] font-semibold text-white px-5 py-3 rounded-4xl w-full mt-">
+          <button className="mt-4 w-full py-3 rounded-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-semibold">
             Get Started Free
           </button>
         </div>
