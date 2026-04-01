73b13f8f27d34ad3d1ad35cd6886a9dd4e0ea13d
Hasan Md Zakaria <zakariak4@gmail.com>
2026-04-01 14:24:22 +0600

    Products component responsive all device

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -8,22 +8,22 @@ const Products = ({ products, carts, setCarts }) => {
   const [isActiveTab, setIsActiveTab] = useState("products");
 
   return (
-    <div className="py-30 max-w-7xl mx-auto">
-      <h3 className="text-[48px] text-[#101727] font-extrabold text-center mt-4">
+    <div className="py-12 md:py20 lg:py-30 max-w-7xl mx-auto">
+      <h3 className="text-2xl md:text-4xl lg:text-[48px] text-[#101727] font-extrabold text-center mt-2 md:mt-4">
         Premium Digital Tools
       </h3>
-      <p className="text-[#627382] leading-5 text-center mb-6">
+      <p className="text-[#627382] text-sm md:text-base leading-6 text-center mb-6 md:mb-10">
         Choose from our curated collection of premium digital products designed
-        <br /> to boost your productivity and creativity.
+        <br className="hidden md:block"/> to boost your productivity and creativity.
       </p>
 
-      <div className="tabs tabs-box justify-center w-84 mx-auto bg-transparent border border-gray-100 rounded-full">
+      <div className="flex justify-center w-full max-w-md mx-auto bg-transparent border border-gray-100 rounded-full p-1">
         <input
           type="radio"
           name="my_tabs_1"
           checked={isActiveTab === "products"}
           onChange={() => setIsActiveTab("products")}
-          className={`tab rounded-full w-40 font-bold ${
+          className={`tab flex-1 py-2 md:py-3 px-4 md:px-6 font-bold text-sm md:text-base rounded-full ${
             isActiveTab === "products"
               ? "bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white"
               : "bg-transparent text-black"
@@ -35,7 +35,7 @@ const Products = ({ products, carts, setCarts }) => {
           name="my_tabs_1"
           checked={isActiveTab === "cart"}
           onChange={() => setIsActiveTab("cart")}
-          className={`tab rounded-full w-40 font-bold ${
+          className={`tab flex-1 py-2 md:py-3 px-4 md:px-6 font-bold text-sm md:text-base rounded-full ${
             isActiveTab === "cart"
               ? "bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white"
               : "bg-transparent text-black"
@@ -45,7 +45,7 @@ const Products = ({ products, carts, setCarts }) => {
       </div>
       
       {isActiveTab === "products" && (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-7 mt-8 md:mt-10">
           {products.map((product) => (
             <ProductCard
               key={product.id}
